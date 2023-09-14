883e5d5d2165a88fb5e0cbc1bbd9df873bad8c56
Taiful2740 <taijeslove@gmail.com>
2023-09-15 00:18:10 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-4xl font-bold text-center my-6">
+      <h1>Course Registration</h1>
+    </div>
+  );
+};
+
+export default Header;
