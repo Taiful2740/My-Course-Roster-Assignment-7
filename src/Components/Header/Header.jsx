62818229b58b94d6c202dedcb4b53dccf75d6725
Taiful2740 <taijeslove@gmail.com>
2023-09-15 11:43:32 +0600

    card layout complate

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
+import { BsCurrencyDollar, BsBook } from "react-icons/bs";
 
 const Header = () => {
   const [cards, setCards] = useState([]);
@@ -11,28 +12,37 @@ const Header = () => {
       .then((res) => res.json())
       .then((data) => setCards(data));
   }, []);
-  // console.log(cards);
+  console.log(cards);
   return (
     <div>
       <div className="text-4xl font-bold text-center my-6">
         <h1>Course Registration</h1>
       </div>
       <div className="flex ">
-        <div className="grid md:w-2/3 grid-cols-3 gap-4">
-          {cards.map((card) => (
-            <div className="card bg-gray-200 shadow-xl">
-              <figure className="px-10 pt-10">
-                <img
-                  src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                  alt="Shoes"
-                  className="rounded-xl"
-                />
+        <div className="grid md:w-3/4 grid-cols-3 gap-6 mx-auto mb-10">
+          {cards.map((card, idx) => (
+            <div key={idx} className="card bg-gray-200 shadow-xl">
+              <figure className="px-4 pt-4">
+                <img src={card.cover} alt="img" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <h2 className="card-title">{card.title}</h2>
+                <p>{card.description}</p>
+                <div className="flex space-x-2 items-center text-center">
+                  <span>
+                    <BsCurrencyDollar></BsCurrencyDollar>
+                  </span>
+
+                  <span className="font-[16px]">Price : {card.price}</span>
+                  <span className="w-[20px] h-[20px]">
+                    <BsBook></BsBook>
+                  </span>
+                  <span>Credit : {card.Credit} </span>
+                </div>
                 <div className="card-actions">
-                  <button className="btn btn-primary">Buy Now</button>
+                  <button className="w-[280px] h-[40px] rounded-[8px] text-white bg-[#2F80ED]">
+                    Select
+                  </button>
                 </div>
               </div>
             </div>
